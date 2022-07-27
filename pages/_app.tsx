bcb9f7401bb425d53010748ae4e04e06d999bd19
Cooper Saye <coopersaye@gmail.com>
2022-07-27 10:28:37 -0700

    Clean up files.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import '../styles/globals.scss';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App(props: AppProps) {
+  const { Component, pageProps } = props;
+
+  return (
+    <>
+      <Head>
+        <title>World of Domains</title>
+        <meta name="description" content="The history of the internet told through domains." />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
-
-export default MyApp
