0401f4708fbf551870ceb00092f6fa825280003c
Cooper Saye <coopersaye@gmail.com>
2022-07-28 14:47:48 -0700

    Create StoryBox component.

---
+++ components/StoryBox.tsx
@@ -0,0 +1,37 @@
+import { useEffect, useRef } from "react";
+import { Story } from "../public/stories";
+
+// distance from center
+const dist = 1.01;
+
+export default function Box(props: Story) {
+  const { rotation } = props;
+
+  const meshRef = useRef<THREE.Mesh>(null);
+
+  // look at origin on start
+  useEffect(() => {
+    if (!meshRef.current) throw 'no mesh';
+    meshRef.current.lookAt(0, 0, 0);
+  }, [rotation]);
+
+  // returns position of box by rotation
+  function getPos(): [number, number, number] {
+    const [yaw, pitch] = rotation;
+    return [
+      dist * Math.sin(yaw) * Math.cos(pitch),
+      dist * Math.sin(pitch),
+      dist * Math.cos(yaw) * Math.cos(pitch),
+    ];
+  }
+
+  return (
+    <mesh
+      ref={meshRef}
+      position={getPos()}
+    >
+      <boxGeometry args={[0.1, 0.1, 0.1]} />
+      <meshStandardMaterial color="white" />
+    </mesh>
+  );
+}
